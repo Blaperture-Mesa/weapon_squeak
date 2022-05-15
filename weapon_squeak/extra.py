a639b2d0623be66e7b76b1fd25642950d6890dc4
AnggaraNothing <anggarayamap@protonmail.com>
2022-05-15 16:28:19 +0700

    Refactoring

--- weapon_squeak/extra.py
+++ weapon_squeak/extra.py
@@ -1,3 +1,22 @@
+from __future__ import annotations
+import typing
+from time import time
+from functools import partial
+__all__ = [
+    "get_epoch"
+    , "split_hostport"
+    , "Singleton"
+]
+
+
+
+get_epoch: typing.Callable[[], int] = lambda: int( time() )
+split_hostport = partial(
+    map
+    , lambda x: x.split(":")
+)
+
+
 # https://stackoverflow.com/a/6798042
 class Singleton (type):
     _instances = {}
